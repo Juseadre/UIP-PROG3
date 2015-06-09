e40c26d907b17c2e5a8448d51370ef5a791bd625
Juseadre <rafael_groves@hotmail.com>
2015-06-08 22:17:22 -0500

    Quiz3 completado

---
+++ laboratorios/sem3/quiz3.py
@@ -0,0 +1,16 @@
+print("Hola! este es el sistema de comisiones\ningrese el nombre del vendedor y la venta")
+nombre)=input("Nombre:	")
+venta1=float(input("venta1:))
+venta2=float(input("venta2:))
+venta3=float(input("venta3:))
+comision=((venta1+venta2+venta3)*12.5)/100
+tventa=(venta1+venta2+venta3)
+tcomisiones=tventas+comision
+print("\nVendedor\tVentas\tComision")
+print(nombre + "\t\t" + str(venta1))
+print(nombre + "\t\t" + str(venta2))
+print(nombre + "\t\t" + str(venta3))
+print("\t\t\t" + str(comision")
+print("\nTotal ventas\t Total+comision")
+print(str(tventas) + "\t\t" + str(tcomision))
+input()
